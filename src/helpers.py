7cee4f8b7f771003d42cd842dd36b2aaf68bbb4b
Imad Rahmouni <rahmounimad@gmail.com>
2018-05-03 17:56:52 +0200

    implement mca

--- src/helpers.py
+++ src/helpers.py
@@ -41,18 +41,15 @@ def columnToMonth(txt):
     month = int((weekNumber*7)/30 + 1)
     return month
 
-def GetMostCorrelatedTo(X_embedded,component,index,n=5,absl = True ):
-    lx = X_embedded.shape[0]+1
+def GetMostCorrelatedTo(X_embedded,component,index,n=10,absl = True ):
     ly = X_embedded.shape[1]+1
     df_Xpca = pd.DataFrame(X_embedded,index=index,columns = np.arange(1,ly))
     return df_Xpca.abs().nlargest(n,component) if absl else df_Xpca.nlargest(n,component)
 
 def Clusters_plot(X,labels, inertia = 0,info=["","",""]):
     plt.figure(figsize=(16,7))
-    cmap = cm.get_cmap('Paired')
     colors = [str(item/255.) for item in labels]
     
-    centroids = np.array(list(set(labels)))
     
     
     plt.suptitle("Clustering Method: (%s,%s,%s)"%(info[0],info[1],info[2]),size=14) 
@@ -60,7 +57,8 @@ def Clusters_plot(X,labels, inertia = 0,info=["","",""]):
     plt.scatter(X[:,0],X[:,1],cmap ="Paired" ,c=colors)
     plt.xlabel("PCA01")    
     plt.ylabel("PCA02")
-    
+
+#     centroids = np.array(list(set(labels)))/
 #     for center in centroids:
 #         #get points of each cluster
 #         cluster = np.where(labels == center)[0]
@@ -93,46 +91,6 @@ def getSSE(samples,centroids):
     return np.sum( (samples-centroids)**2)
 
 
-from matplotlib.patches import Ellipse
-def plot_cov_ellipse(cov, pos, nstd=2, ax=None, **kwargs):
-    """
-    Plots an `nstd` sigma error ellipse based on the specified covariance
-    matrix (`cov`). Additional keyword arguments are passed on to the 
-    ellipse patch artist.
-
-    Parameters
-    ----------
-        cov : The 2x2 covariance matrix to base the ellipse on
-        pos : The location of the center of the ellipse. Expects a 2-element
-            sequence of [x0, y0].
-        nstd : The radius of the ellipse in numbers of standard deviations.
-            Defaults to 2 standard deviations.
-        ax : The axis that the ellipse will be plotted on. Defaults to the 
-            current axis.
-        Additional keyword arguments are pass on to the ellipse patch.
-
-    Returns
-    -------
-        A matplotlib ellipse artist
-    """
-    def eigsorted(cov):
-        vals, vecs = np.linalg.eigh(cov)
-        order = vals.argsort()[::-1]
-        return vals[order], vecs[:,order]
-
-    if ax is None:
-        ax = plt.gca()
-
-    vals, vecs = eigsorted(cov)
-    theta = np.degrees(np.arctan2(*vecs[:,0][::-1]))
-
-    # Width and height are "full" widths, not radius
-    width, height = 2 * nstd * np.sqrt(vals)
-    ellip = Ellipse(xy=pos, width=width, height=height, angle=theta, **kwargs)
-
-    ax.add_artist(ellip)
-    return ellip
-
 
 def Cluster_series_plot(data_df,cluster_df):
     
@@ -161,3 +119,59 @@ def Cluster_series_plot(data_df,cluster_df):
     plt.tight_layout()
     #plt.show()
     return clusters_array
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # from matplotlib.patches import Ellipse
+# def plot_cov_ellipse(cov, pos, nstd=2, ax=None, **kwargs):
+#     """
+#     Plots an `nstd` sigma error ellipse based on the specified covariance
+#     matrix (`cov`). Additional keyword arguments are passed on to the 
+#     ellipse patch artist.
+
+#     Parameters
+#     ----------
+#         cov : The 2x2 covariance matrix to base the ellipse on
+#         pos : The location of the center of the ellipse. Expects a 2-element
+#             sequence of [x0, y0].
+#         nstd : The radius of the ellipse in numbers of standard deviations.
+#             Defaults to 2 standard deviations.
+#         ax : The axis that the ellipse will be plotted on. Defaults to the 
+#             current axis.
+#         Additional keyword arguments are pass on to the ellipse patch.
+
+#     Returns
+#     -------
+#         A matplotlib ellipse artist
+#     """
+#     def eigsorted(cov):
+#         vals, vecs = np.linalg.eigh(cov)
+#         order = vals.argsort()[::-1]
+#         return vals[order], vecs[:,order]
+
+#     if ax is None:
+#         ax = plt.gca()
+
+#     vals, vecs = eigsorted(cov)
+#     theta = np.degrees(np.arctan2(*vecs[:,0][::-1]))
+
+#     # Width and height are "full" widths, not radius
+#     width, height = 2 * nstd * np.sqrt(vals)
+#     ellip = Ellipse(xy=pos, width=width, height=height, angle=theta, **kwargs)
+
+#     ax.add_artist(ellip)
+#     return ellip
